57ef36a4f7e6c55a423ae55134f95e3384d447a0
SBCai <shibincai666@gmail.com>
2024-02-11 18:01:37 -0500

    finished lab 3

---
+++ src/Labs/a3/index.tsx
@@ -0,0 +1,25 @@
+import Classes from "./Classes";
+import Styles from "./Styles";
+import JavaScript from "./JavaScript";
+import PathParameters from "./routing/PathParameters";
+import ConditionalOutput from "./ConditionalOutput";
+import Highlight from "./Highlight";
+import TodoList from "./todos/TodoList";
+
+function Assignment3() {
+ return (
+   <div>
+     <h1>Assignment 3</h1>
+     <TodoList/>
+     <ConditionalOutput/>
+     ...
+     <Highlight>
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+             vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+     </Highlight>
+
+     <JavaScript/>
+   </div>
+ );}
+
+export default Assignment3;
