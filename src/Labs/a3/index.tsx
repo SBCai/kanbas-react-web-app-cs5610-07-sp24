5148dbce367f68ded1c94ba2f6d596eba34ec7a2
SBCai <shibincai666@gmail.com>
2024-02-27 21:54:46 -0500

    finished lab 4

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -5,11 +5,22 @@ import PathParameters from "./routing/PathParameters";
 import ConditionalOutput from "./ConditionalOutput";
 import Highlight from "./Highlight";
 import TodoList from "./todos/TodoList";
+import { useSelector } from "react-redux";
+import { LabState } from "../store";
 
 function Assignment3() {
+  const { todos } = useSelector((state: LabState) => state.todosReducer);
  return (
    <div>
      <h1>Assignment 3</h1>
+     <ul className="list-group">
+             {todos.map((todo) => (
+               <li className="list-group-item" key={todo.id}>
+                 {todo.title}
+               </li>
+             ))}
+     </ul>
+
      <TodoList/>
      <ConditionalOutput/>
      ...
